55cfa3ad4713f746e944cf56e27b111035e71b9b
Yohan Lasorsa <yohanlasorsa.thales@gmail.com>
2017-07-10 11:23:01 +0200

    Fixed build

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 
-import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
 @NgModule({
@@ -11,7 +10,6 @@ import { LoginComponent } from './login.component';
     CommonModule,
     ReactiveFormsModule,
     TranslateModule,
-    LoginRoutingModule
   ],
   declarations: [
     LoginComponent
