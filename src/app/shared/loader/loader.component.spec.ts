2cc17c997fab0c516f2e62a6f2cc128a590bd03f
Yohan Lasorsa <yohanlasorsa.thales@gmail.com>
2017-10-25 14:41:31 +0200

    Updated from generator v2.3.0

--- src/app/shared/loader/loader.component.spec.ts
+++ src/app/shared/loader/loader.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from 'ionic-angular';
 
 import { LoaderComponent } from './loader.component';
-import { IonicModule } from 'ionic-angular';
 
 describe('LoaderComponent', () => {
   let component: LoaderComponent;
