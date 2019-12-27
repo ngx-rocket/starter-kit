f9c74f0d644f2778f0e6d39167cf3e40e2383c7e
sanjeet kumar <sanjeet.kumar2@delhivery.com>
2019-12-27 14:15:16 +0530

    hot module reloading fixes when local serve

--- src/hmr.ts
+++ src/hmr.ts
@@ -3,17 +3,19 @@ import { createNewHosts } from '@angularclass/hmr';
 
 export function hmrBootstrap(module: any, bootstrap: () => Promise<NgModuleRef<any>>) {
   let ngModule: NgModuleRef<any>;
-  module.hot.accept();
 
   bootstrap()
     .then(mod => (ngModule = mod))
     .catch(err => console.error(err));
 
-  module.hot.dispose(() => {
-    const appRef: ApplicationRef = ngModule.injector.get(ApplicationRef);
-    const elements = appRef.components.map(c => c.location.nativeElement);
-    const makeVisible = createNewHosts(elements);
-    ngModule.destroy();
-    makeVisible();
-  });
+  if (module && module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => {
+      const appRef: ApplicationRef = ngModule.injector.get(ApplicationRef);
+      const elements = appRef.components.map(c => c.location.nativeElement);
+      const makeVisible = createNewHosts(elements);
+      ngModule.destroy();
+      makeVisible();
+    });
+  }
 }
