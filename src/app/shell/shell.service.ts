bfec0b09065a72f59803cc8fbcf5b2638247c922
senglish35 <scott.h.english@gmail.com>
2020-11-10 22:17:35 -0600

    Disabled authentication

--- src/app/shell/shell.service.ts
+++ src/app/shell/shell.service.ts
@@ -17,7 +17,7 @@ export class Shell {
       path: '',
       component: ShellComponent,
       children: routes,
-      canActivate: [AuthenticationGuard],
+      // canActivate: [AuthenticationGuard],
       // Reuse ShellComponent instance when navigating between child views
       data: { reuse: true },
     };
