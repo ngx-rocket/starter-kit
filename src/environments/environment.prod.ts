3fb99c7fb5d2172cafea7dfd7a83ba95f633623c
cirebot.github@gmail.com <ci-rebot>
2021-01-14 09:46:56 +0000

    Updated from generator v9.2.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,6 @@ import { env } from './.env';
 
 export const environment = {
   production: true,
-  hmr: false,
   version: env.npm_package_version,
   serverUrl: 'https://api.chucknorris.io',
   defaultLanguage: 'en-US',
